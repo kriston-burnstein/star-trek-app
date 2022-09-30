0f27fab76f5f6566902ec41f3aabd1482e2e24f7
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-09-30 19:34:12 -0400

    intial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//Client side creating with Mayan Wolfe streaming
+
+document.getElementById('getButton').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const alienName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://my-first-api-to-heroku.herokuapp.com/api/${alienName}`)
+        const data = await response.json()
+        console.log(data)
+        document.getElementById('alienName').innerText = data.speciesName
+        document.getElementById('alienWorld').innerText = data.homeworld
+        document.getElementById('alienFeatures').innerText = data.features
+        document.getElementById('alienFacts').innerText = data.interestingFact
+        document.getElementById('alienExamples').innerText = data.notableExamples
+        document.getElementById('alienImage').src = data.image
+        document.getElementById('alienCaption').innerText = data.speciesName
+    }catch(error){
+        console.log(error)
+    }
+}
+
+//https://my-first-api-to-heroku.herokuapp.com/ is my heroku Star Trek API
